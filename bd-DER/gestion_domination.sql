33fccfba5778cc09e35fa164fe9dbc5bc630dd8c
Sebastian Giannini <sebastiangiannini95@gmail.com>
2023-11-04 18:05:26 -0300

    Ultima actualizacion DER

---
+++ bd-DER/gestion_domination.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuario (nombre_usuario, nombre, apellido, email, password, celular, administrador_idadministrador)
+VALUES
+  ('usuario1', 'Nombre1', 'Apellido1', 'usuario1@example.com', 'contraseña1', '1234567891', 1),
+  ('usuario2', 'Nombre2', 'Apellido2', 'usuario2@example.com', 'contraseña2', '1234567892', 1),
+  ('usuario3', 'Nombre3', 'Apellido3', 'usuario3@example.com', 'contraseña3', '1234567893', 1),
+  ('usuario4', 'Nombre4', 'Apellido4', 'usuario4@example.com', 'contraseña4', '1234567894', 1);
